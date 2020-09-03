32665be55a7bcacd4adf018b67504b982e3d907e
xeyzo <schneidergw@yahoo.com>
2020-09-03 08:58:41 +0700

    add 07

--- 07.py
+++ 07.py
@@ -1,7 +1,14 @@
+def fix_odd_even_indices(source_array):
+    odd_numbers = sorted([n for n in source_array if n%2!=0])
+    c = 0
+    res = []
+    for i in source_array:
+        if i %2!=0:
+            res.append(odd_numbers[c])
+            c += 1
+        else:
+            res.append(i)
+    print(res)
+
 numbers = [9,4,2,4,1,5,3,0]
-
-
-odd_numbers = [n for n in numbers if n%2!=0]
-odd_numbers = sorted(odd_numbers)
-
-print(odd_numbers)
+fix_odd_even_indices(numbers)
